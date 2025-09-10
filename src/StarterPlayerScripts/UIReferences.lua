41e126d398d668b08793b1eb7087ccf2ca7d9f97
Nov19 <89353728+Nov19@users.noreply.github.com>
2025-09-10 23:39:13 +0800

    Initial commit

---
+++ src/StarterPlayerScripts/UIReferences.lua
@@ -0,0 +1,55 @@
+--[[
+    Services
+    Naming convention: Ser_???
+]]
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+--[[
+    Modules
+    Naming convention: ???Module
+]]
+local UIReference = {}
+
+--[[
+    Remote events
+    Naming convention: RE_???
+]]
+
+--[[
+    Bindable events
+    Naming convention: BE_???
+]]
+
+--[[
+    Remote functions
+    Naming convention: RF_???
+]]
+
+--[[
+    Bindable functions
+    Naming convention: BF_???
+]]
+
+--[[
+    Tables
+    Naming convention: Tab_???
+]]
+
+--[[
+    References & Parameters
+]]
+
+--[[
+    Functions
+]]
+
+--[[
+    Event connections
+    Conventional order: Remote events -> Bindable events -> Remote functions -> Bindable functions
+]]
+
+--[[
+    Code execution
+]]
+
+return UIReference
