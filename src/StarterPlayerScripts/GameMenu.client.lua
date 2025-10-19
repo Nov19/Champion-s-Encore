35850e1353efca5ebbfc8f3bdc884328a1c1018d
Nov19 <jlnov19@yahoo.com>
2025-10-19 16:49:19 +0800

    Enhance Game Menu Functionality with Camera and Blur Effects
    Updated the GameMenu.client.lua script to include new camera handling and visual effects. Implemented a function to hide the player's character until the game starts and adjusted the camera settings upon game load. Added a blur effect transition when the Play button is activated, improving the visual experience during the transition from the Start Menu to the game. Integrated the Communication module to handle button activation events.

--- src/StarterPlayerScripts/GameMenu.client.lua
+++ src/StarterPlayerScripts/GameMenu.client.lua
@@ -2,8 +2,10 @@
     Services
     Naming convention: ???
 ]]
-
+local Lighting = game:GetService("Lighting")
+local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
 
 --[[
     Modules
@@ -11,6 +13,8 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 ]]
 
 local UIReferences = require(script.Parent.UIReferences)
+local Communication = require(ReplicatedStorage.Modules.Communication)
+local ModelHelper = require(ReplicatedStorage.Modules.Helpers.ModelHelper)
 
 --[[
     Tables
@@ -21,6 +25,12 @@ local UIReferences = require(script.Parent.UIReferences)
     References & Parameters
 ]]
 
+local DEFAULT_BLUR_SIZE = 36
+local DEFAULT_BLUR_TWEEN_INFO = TweenInfo.new(1.3, Enum.EasingStyle.Linear)
+
+local currentCamera = workspace.CurrentCamera
+local startMenuCameraCFrame = workspace:WaitForChild("StartMenuCameraPart").CFrame
+
 --[[
     Local functions
 ]]
@@ -29,6 +39,14 @@ local UIReferences = require(script.Parent.UIReferences)
     Functions
 ]]
 
+local function OnGameLoaded()
+	Players.LocalPlayer.Character.Parent = nil -- Hide the player's character until the game starts
+
+	-- Set the camera to Scriptable
+	currentCamera.CameraType = Enum.CameraType.Scriptable
+	currentCamera.CFrame = startMenuCameraCFrame
+end
+
 --[[
     Event connections
     Conventional order: Remote events -> Bindable events -> Remote functions -> Bindable functions
@@ -36,9 +54,24 @@ local UIReferences = require(script.Parent.UIReferences)
 
 UIReferences.StartMenuUI.PlayBtn.Activated:Connect(function()
 	UIReferences.Guis.StartMenuUI.Enabled = false
-	UIReferences.Guis.ControllerUI.Enabled = true
+	Players.LocalPlayer.Character.Parent = workspace -- Show the player's character
+	currentCamera.CameraType = Enum.CameraType.Custom
+
+	task.spawn(function()
+		local blur = Lighting:FindFirstChild("Blur")
+		local tween = TweenService:Create(blur, DEFAULT_BLUR_TWEEN_INFO, {
+			Size = 0,
+		})
+		tween:Play()
+		tween.Completed:Wait()
+		blur.Enabled = false
+		blur.Size = DEFAULT_BLUR_SIZE
+	end)
+
+	Communication.Fire("PlayBtnActivated")
 end)
 
 --[[
     Code execution
 ]]
+OnGameLoaded()
