ff54a1b14be9e35cfe43e3c16279388f7214721c
Nov19 <jlnov19@yahoo.com>
2025-10-19 14:45:30 +0800

    Add Communication Helper Module for Event Initialization

---
+++ src/ServerScriptService/CommunicationHelper.server.lua
@@ -0,0 +1,65 @@
+--[[
+    Services
+    Naming convention: ???
+]]
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--[[
+    Modules
+    Naming convention: ???
+]]
+
+local Communication = require(ReplicatedStorage.Modules.Communication)
+
+--[[
+    Tables
+    Naming convention: ???
+]]
+
+local BindableEvents = {
+	"PlayBtnActivated",
+}
+
+local RemoteEvents = {}
+
+local RemoteFunctions = {}
+
+local BindableFunctions = {}
+
+--[[
+    References & Parameters
+]]
+
+--[[
+    Local functions
+]]
+
+--[[
+    Functions
+]]
+
+local function InitializeCommunication()
+	for _, bindableEvent in pairs(BindableEvents) do
+		Communication.GetBindableEvent(bindableEvent)
+	end
+	for _, remoteEvent in pairs(RemoteEvents) do
+		Communication.GetRemoteEvent(remoteEvent)
+	end
+	for _, remoteFunction in pairs(RemoteFunctions) do
+		Communication.GetRemoteFunction(remoteFunction)
+	end
+	for _, bindableFunction in pairs(BindableFunctions) do
+		Communication.GetBindableFunction(bindableFunction)
+	end
+end
+
+--[[
+    Event connections
+    Conventional order: Remote events -> Bindable events -> Remote functions -> Bindable functions
+]]
+
+--[[
+    Code execution
+]]
+InitializeCommunication()
